8054156443752da517bf12531386d8a6c4e437de
maxwell <github email address>
2018-07-04 12:21:36 -0400

    fixed webpack issue

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import chai from 'chai'
 import './App.css';
-import chaiEnzyme from 'chai-enzyme'
 
-chai.use(chaiEnzyme()) // Note the invocation at the end
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
