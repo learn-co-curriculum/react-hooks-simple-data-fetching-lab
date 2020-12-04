982fed2c2cfa37851da1bf1c874c3571868e6149
ihollander <ianholla@gmail.com>
2020-12-04 14:22:23 -0500

    Added solution

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
-// create your App component here
+import React, { useState, useEffect } from 'react'
+
+function App() {
+  const [peopleInSpace, setPeopleInSpace] = useState([])
+
+  useEffect(() => {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(data => {
+        setPeopleInSpace(data.people)
+      })
+  }, []) 
+  // use an empty dependencies array, so we only run the fetch request ONCE
+
+  return (
+    <div>
+      {peopleInSpace.map(person => person.name)}
+    </div>
+  )
+}
+
+export default App
