74aed0450429c114d9f73a3f4c12ef999edb21f2
ihollander <ianholla@gmail.com>
2020-12-27 14:25:49 -0500

    Remove unused packages

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import { server } from "./mocks/server";
+import { server } from "../mocks/server";
 
-import App from "../App";
+import App from "../components/App";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-it("displays the dog image after fetching", async () => {
+test("displays the dog image after fetching", async () => {
   render(<App />);
   const img = await screen.findByAltText("A Random Dog");
   expect(img.src).toBe(
@@ -17,7 +17,7 @@ it("displays the dog image after fetching", async () => {
   );
 });
 
-it("displays a loading message before fetching", async () => {
+test("displays a loading message before fetching", async () => {
   render(<App />);
   expect(screen.queryByText(/Loading/)).toBeInTheDocument();
 
