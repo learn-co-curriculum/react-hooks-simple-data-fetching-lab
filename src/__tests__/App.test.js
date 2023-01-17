4231eea8dd62e581f7ab1888ff36d5cf4c549863
Jerrica Bobadilla <jbobadilla27@gmail.com>
2023-01-17 11:42:42 -0800

    Add fetch polyfill

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "whatwg-fetch";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { server } from "../mocks/server";
