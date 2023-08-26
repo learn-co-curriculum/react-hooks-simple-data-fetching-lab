961bad9eda9bb6e0cf763b24f17f42145c421c33
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-08-25 21:33:32 -0400

    add solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
-// create your App component here
+import { useState, useEffect } from "react"
+
+const App = () => {
+  const [dogPic, setDogPic] = useState(null)
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
+    .then((dogPic) => {
+      setDogPic(dogPic.message)
+    })
+
+
+  }, [])
+  if(!dogPic) return <p>Loading</p>
+  return(
+  <img src={dogPic} alt="A random Dog"/>
+
+  )
+}
+export default App
